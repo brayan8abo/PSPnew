200d282d4668745c6ea700d8bd42c85f7096d020
brayan8abo <138830328+brayan8abo@users.noreply.github.com>
2025-01-30 19:50:22 +0100

    Update

--- hiloIdentificador.java
+++ hiloIdentificador.java
@@ -1,26 +1,30 @@
 public class hiloIdentificador extends Thread {
 
-    public void run () {
+    public void run() {
         System.out.println(
-            "Dentro del HILO: " + Thread.currentThread().getName() + "\n\tprioridad: " + Thread.currentThread().getPriority() + "\n\tID: " + Thread.currentThread().getId()+ "\n\tHilos activos: " + Thread.currentThread().activeCount();
-            );
+                "Dentro del HILO: " + Thread.currentThread().getName() + "\n\tprioridad: "
+                        + Thread.currentThread().getPriority() + "\n\tID: " + Thread.currentThread().getId()
+                        + "\n\tHilos activos: " + Thread.currentThread().activeCount());
     }
 
     public static void main(String[] args) {
-        Thread.currentThread().setName("Principal"); //Nombre a main
+        Thread.currentThread().setName("Principal"); // Nombre a main
         System.out.println(Thread.currentThread().getName());
         System.out.println(Thread.currentThread().toString());
 
         hiloIdentificador h = null;
 
         for (int i = 0; i < 3; i++) {
-            h = new hiloIdentificador(); //creamos el hilo
-            h.setName("HILO"+i); //damos nombre al hilo
-            h.setPriority(i+1); //damos prioridad
-            h.start(); //iniciamos el hilo 
+            h = new hiloIdentificador(); // creamos el hilo
+            h.setName("HILO" + i); // damos nombre al hilo
+            h.setPriority(i + 1); // damos prioridad
+            h.start(); // iniciamos el hilo
 
             System.out.println("Información del " + h.getName() + ": " + h.toString());
         }
+
+        System.out.println("3 HILOS CREADOS...");
+        System.out.println("Hilos activos: " + Thread.activeCount());
     }
 
 }
